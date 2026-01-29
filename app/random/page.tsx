90efba435026a9e07aab1e2234560ae2b6eea17e
Tanveer Najib <tanveernajib@gmail.com>
2026-01-29 13:32:21 +0000

    Add random page for random stuff

---
+++ app/random/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { ShowFinishDate } from "@/components/ShowFinishDate";
+
+export default function Random() {
+    return (
+        <ShowFinishDate />
+    );
+}
---
+++ components/ShowFinishDate.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export function ShowFinishDate() {
+    const CURRENT_SHOW = "Gintama"
+    const EPISODE_COUNT = 367
+    const NUM_EPS_PER_DAY = 2
+
+    const [finishDate, setFinishDate] = useState<Date | null>(null)
+    const [episodesLeft, setEpisodesLeft] = useState<number>(0)
+    const [amountOfDays, setAmountOfDays] = useState<number>(0)
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const headers = {
+                    "Content-Type": "application/json",
+                    "trakt-api-version": "2",
+                    "trakt-api-key": "80385a95267fd85c22d380c508ee1aa687fc0aba899e3433a7df977ce0062e56",
+                };
+
+                const watchedRes = await fetch(
+                    `https://api.trakt.tv/users/crxssed/history/shows/gintama?limit=400`,
+                    { headers }
+                );
+
+                if (!watchedRes.ok) {
+                    throw new Error("Failed to fetch watched data");
+                }
+                const watchedHistoryItems = await watchedRes.json();
+                // TODO: This assumes that I watch each episode only once
+                const watchedEpisodes = watchedHistoryItems.length
+                const amountLeft = EPISODE_COUNT - watchedEpisodes
+                const amountOfDays = amountLeft / NUM_EPS_PER_DAY
+                const finishDate = new Date(Date.now() + amountOfDays * 24 * 60 * 60 * 1000)
+                setFinishDate(finishDate)
+                setEpisodesLeft(amountLeft)
+                setAmountOfDays(amountOfDays)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    if (finishDate === null) return <p>Loading…</p>;
+
+    return (
+        <div>
+            <p><b>{CURRENT_SHOW}</b>: episodes left: {episodesLeft}</p>
+            <p>
+                Finish date ({NUM_EPS_PER_DAY} / day):{" "}
+                <strong>
+                    {finishDate.toLocaleDateString(undefined, {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                    })} ({amountOfDays} days, ~{amountOfDays / 30} months)
+                </strong>
+            </p>
+        </div>
+    );
+}
